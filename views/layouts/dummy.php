6e3cfcb4f369da0eee19eed4be17c10423a4079c
carono <info@carono.ru>
2017-08-01 20:50:21 +0500

    fix

---
+++ views/layouts/dummy.php
@@ -0,0 +1,18 @@
+<div id="wrapper">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Сайт в разработке</h1>
+                <h2 class="subtitle">Тут будет моя домашняя страница</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-lg-offset-3">Но это не точно</div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h1><a href="mailto:info@carono.ru">info@carono.ru</a></h1>
+            </div>
+        </div>
+    </div>
+</div>
