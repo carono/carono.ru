c393ddc26b9b1154194d85e4bf51e2326d512e8d
carono <info@carono.ru>
2017-08-01 20:35:41 +0500

    env

---
+++ environments/dev/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=carono',
+    'username' => 'carono',
+    'password' => 'xyCwQUXX',
+    'charset' => 'utf8',
+];
