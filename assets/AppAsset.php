28e4cbff1805299c7079f262fd3efaa4d3707078
carono <info@carono.ru>
2017-08-02 17:31:10 +0500

    refs

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,9 +18,24 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        '//fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic',
+        '//fonts.googleapis.com/css?family=Montserrat:400,700',
+        'plugins/bootstrap/css/bootstrap.min.css',
+        'plugins/font-awesome/css/font-awesome.css',
+        'plugins/github-calendar/dist/github-calendar.css',
+        'plugins/github-activity/src/github-activity.css',
+        '//cdnjs.cloudflare.com/ajax/libs/octicons/2.0.2/octicons.min.css',
+        'css/styles.css',
     ];
     public $js = [
+        "plugins/bootstrap/js/bootstrap.min.js",
+        "//cdnjs.cloudflare.com/ajax/libs/es6-promise/3.0.2/es6-promise.min.js",
+        "plugins/jquery-rss/dist/jquery.rss.min.js",
+        "plugins/github-calendar/dist/github-calendar.min.js",
+        "plugins/github-activity/src/github-activity.js",
+        "js/main.js",
+        "//cdnjs.cloudflare.com/ajax/libs/fetch/0.10.1/fetch.min.js",
+        "//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min.js",
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- views/layouts/dummy.php
+++ views/layouts/dummy.php
@@ -1,18 +1,28 @@
-<div id="wrapper">
-    <div class="container text-center">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Сайт в разработке</h1>
-                <h2 class="subtitle">Тут будет моя домашняя страница</h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-6 col-lg-offset-3">Но это не точно</div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <h1><a href="mailto:info@carono.ru">info@carono.ru</a></h1>
-            </div>
+<div class="container sections-wrapper">
+    <div class="row">
+        <div class="primary col-lg-12">
+            <section class="about section">
+                <div class="section-inner">
+                    <div id="wrapper">
+                        <div class="container text-center">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h1>Сайт в разработке</h1>
+                                    <h2 class="subtitle">Тут будет моя домашняя страница</h2>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6 col-lg-offset-3">Но это не точно</div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h1><a href="mailto:info@carono.ru">info@carono.ru</a></h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
 </div>
